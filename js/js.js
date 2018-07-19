7a4c1f1aab56606ce865e19c7d4c2b684e6f767b
Joe <g2bb@aol.com>
2018-07-19 13:52:32 -0500

    Initial Commit

---
+++ js/js.js
@@ -0,0 +1,9 @@
+function ME() {
+    var x;
+    if (confirm("This is a link to an external website.  Are you sure you want to continue?") == true) {
+        x = "";
+        alert("NOPE, TOO BAD!!!  You're staying on my website!");
+    } else {
+
+    }
+}
